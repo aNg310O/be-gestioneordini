a0a3742b19b2d6d32a81c0fb920891079fbb3624
aNg310O <angelo.deprosperis@gmail.com>
2025-01-12 16:01:13 +0100

    add product api for product table (get, enable, disable)

--- controllers/productController.js
+++ controllers/productController.js
@@ -20,6 +20,57 @@ const createProduct = async (req, res) => {
   }
 };
 
+const getAllProducts = async (req, res) => {
+  try {
+    const products = await prodotti.findAll();
+    res.json(products);
+  } catch (error) {
+    console.error("Errore nel recupero dei prodotti:", error);
+    res.status(500).json({ message: "Errore del server" });
+  }
+};
+
+const disableProduct = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await prodotti.findByPk(id);
+
+    if (!product) {
+      return res.status(404).json({ message: "Prodotto non trovato" });
+    }
+
+    product.valid = false;
+    await product.save();
+
+    res.json({ message: "Prodotto disabilitato con successo" });
+  } catch (error) {
+    console.error("Errore nella disabilitazione del prodotto:", error);
+    res.status(500).json({ message: "Errore del server" });
+  }
+};
+
+const enableProduct = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await prodotti.findByPk(id);
+
+    if (!product) {
+      return res.status(404).json({ message: "Prodotto non trovato" });
+    }
+
+    product.valid = true;
+    await product.save();
+
+    res.json({ message: "Prodotto abilitato con successo" });
+  } catch (error) {
+    console.error("Errore nell'abilitazione del prodotto:", error);
+    res.status(500).json({ message: "Errore del server" });
+  }
+};
+
 module.exports = {
   createProduct,
+  getAllProducts,
+  disableProduct,
+  enableProduct,
 };
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,10 +1,18 @@
 // routes/productRoutes.js
 const express = require("express");
-const { createProduct } = require("../controllers/productController");
+const {
+  createProduct,
+  getAllProducts,
+  disableProduct,
+  enableProduct,
+} = require("../controllers/productController");
 const { verifyToken, isAdmin } = require("../middlewares/authMiddleware");
 
 const router = express.Router();
 
 router.post("/", verifyToken, isAdmin, createProduct);
+router.get("/", verifyToken, isAdmin, getAllProducts);
+router.put("/:id/disable", verifyToken, isAdmin, disableProduct);
+router.put("/:id/enable", verifyToken, isAdmin, enableProduct);
 
 module.exports = router;
