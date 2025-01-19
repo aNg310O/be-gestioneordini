4c973383344c37157b1e9f797a5280f5da670935
aNg310O <angelo.deprosperis@gmail.com>
2025-01-19 18:06:14 +0100

    set getProduce accessible to all roles

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -11,7 +11,7 @@ const { verifyToken, isAdmin } = require("../middlewares/authMiddleware");
 const router = express.Router();
 
 router.post("/", verifyToken, isAdmin, createProduct);
-router.get("/", verifyToken, isAdmin, getAllProducts);
+router.get("/", verifyToken, getAllProducts);
 router.put("/:id/disable", verifyToken, isAdmin, disableProduct);
 router.put("/:id/enable", verifyToken, isAdmin, enableProduct);
 
